332f3d95911980be165d51b847dc7ac2920fef40
NikhilRmOrNikhilDev <95686653+NikhilRmOrNikhilDev@users.noreply.github.com>
2022-01-06 09:19:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("My first console screen project");
+var f_name = "Nikhil";
+var l_name = "Mudaliar";
+var a = 15;
+var b = 15;
+var add = a+b;
+var full_name = f_name.concat(" ",l_name)
+console.log(full_name);
+console.log(a);
+console.log(b);
+console.log("15 + 15 = " + add);
